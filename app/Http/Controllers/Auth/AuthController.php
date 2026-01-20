96c446992eb507061744a1ae225f201e418abbd9
Matheus Silva <ms25022003@gmail.com>
2026-01-20 19:01:08 -0300

    feat(auth): update logout method to use LogoutRequest

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AuthenticateRequest;
+use App\Http\Requests\LogoutRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
@@ -30,7 +30,7 @@ class AuthController extends Controller
         ]);
     }
 
-    public function logout(Request $request): RedirectResponse
+    public function logout(LogoutRequest $request): RedirectResponse
     {
         Auth::logout();
 
